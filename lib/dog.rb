0dda36034f31e7e653a0d4017114d6bd053a043f
Catherine Salazar <catcreates13@gmail.com>
2018-09-10 02:12:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,11 +27,11 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = self.new(name, breed)
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.grade = row[2]
-    new_dog
+    self.new (
+    row[0]
+    row[1]
+    row[2]
+    )
   end
 
 
