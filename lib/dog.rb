b3d9b585b7be924f001855164520bb85b775a0b5
Catherine Salazar <catcreates13@gmail.com>
2018-09-10 02:11:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,14 +34,6 @@ class Dog
     new_dog
   end
 
-  def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
-  end
-
 
   def save
 
