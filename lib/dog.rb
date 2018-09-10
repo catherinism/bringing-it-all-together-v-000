2fa84c7a9f8fd179f9f486a5a527fd6e795acd3c
Catherine Salazar <catcreates13@gmail.com>
2018-09-10 02:36:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,10 +37,17 @@ class Dog
     )
  end
 
- def self.find_by_name
+ def self.find_by_name(name)
+  sql = <<-SQL
+         SELECT *
+         FROM dogs
+         WHERE name = ?
+         LIMIT 1
+       SQL
 
- end
-
-
+       DB[:conn].execute(sql, name).map do |row|
+         self.new_from_db(row)
+       end.first
+     end
 
 end
