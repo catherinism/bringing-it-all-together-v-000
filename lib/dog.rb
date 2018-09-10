03555742d06ee237bbcd2306ab2eef7305b89387
Catherine Salazar <catcreates13@gmail.com>
2018-09-10 02:35:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,25 +29,16 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-#   def save
-#     if self.id
-#       self.update
-#     else
-#      sql = <<-SQL
-#      INSERT INTO dogs (name, breed)
-#      VALUES (?, ?)
-#      SQL
-#      DB[:conn].execute(sql, self.name, self.breed)
-#      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-#    end
-# end
-
   def self.new_from_db(row)
     self.new(
     id: row[0],
     name: row[1],
     breed: row[2]
     )
+ end
+
+ def self.find_by_name
+
  end
 
 
